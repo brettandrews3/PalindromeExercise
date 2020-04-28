ede3d6b29a9fc8c5d35a79c6a4083834bebb9c37
Brett Andrews <brettandrews3@gmail.com>
2020-04-28 09:58:52 -0500

    Class worked through

--- PalindromeExercise/PalindromeExercise/Palindrome.cs
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Linq;
 namespace PalindromeExercise
 {
-    public class Palindrome
+    public class WordSmith
     {
-        public bool IsAPalindrome(string word) => bool;
+        public bool IsAPalindrome(string word) //to start scope, look at return type
+        {
+            var reversedWord = word.Reverse();
+            return word.ToLower().SequenceEqual(reversedWord); /* Determine whether two
+            sequences are equal by returning the reverse */
+
+            //var reversed = "";
+            //for(int i = word.Length - 1; i >= 0; i--) // start at end of word, write backwards
+            //{
+              //  reversed += word[i]; // print each letter of word
+            //}
+            //if(reversed.ToLower() == word.ToLower())
+            //{
+              //  return true;
+            //}
+            //return false;
+            // reverse the word
+            // compare the original with reversed word
+            // return whether they're the same
+        }
     }
 }
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("toot", true)]
+        [InlineData("boob", true)]
+        [InlineData("return", false)]
+        [InlineData("Leffel", true)]
+        [InlineData("night", false)]
+        [InlineData("shift", false)]
+        [InlineData("tacocat", true)]
+        public void MyTest(string word, bool expected)
         {
-
+            // Arrange
+            var tester = new WordSmith(); // Creating new instance of Palindrome class
+            // Act
+            var actual = tester.IsAPalindrome(word);
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
